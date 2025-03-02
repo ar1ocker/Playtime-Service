1e36652f9177718b136db02f4dc894ffa4f7de44
Ar1ocker <ar1oc@yandex.ru>
2025-03-02 22:27:08 +0300

    Улучшения админки

--- playtime_service/playtime/admin.py
+++ playtime_service/playtime/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import BattlemetricsSetPath, Playtime, PlaytimeGetPath
 
@@ -22,6 +23,7 @@ class PlaytimeAdmin(admin.ModelAdmin):
     list_display = [
         "id",
         "steam_id",
+        "steam_profile",
         "game_id",
         "get_steam_playtime_hours",
         "get_bm_playtime_hours",
@@ -39,6 +41,8 @@ class PlaytimeAdmin(admin.ModelAdmin):
     ]
     sortable_by = ["id", "game_id", "created_at", "updated_at", "get_steam_playtime_hours", "get_bm_playtime_hours"]
 
+    date_hierarchy = "created_at"
+
     @admin.display(description="Игровое время по Steam (часы)")
     def get_steam_playtime_hours(self, obj):
         return int(obj.steam_playtime / 60 / 60) if obj.steam_playtime else None
@@ -50,6 +54,10 @@ class PlaytimeAdmin(admin.ModelAdmin):
         return int(obj.bm_playtime / 60 / 60) if obj.bm_playtime else None
 
     get_bm_playtime_hours.admin_order_field = "-bm_playtime"
+
+    @admin.display(description="Профиль в Steam")
+    def steam_profile(self, obj):
+        return format_html('<a href="https://steamcommunity.com/profiles/{}" target="_blank">Профиль</a>', obj.steam_id)
 
 
 @admin.register(BattlemetricsSetPath)
